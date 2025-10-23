1ccb9f94c83815cb0616c34532ff1f04b44c7330
MariVS0912 <mvargass3@eafit.edu.co>
2025-10-22 23:02:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -19,13 +19,10 @@ try:
 except:
     pass
 
-st.subheader("Una pequeña Fábula.")
-st.write('¡Ay! -dijo el ratón-. El mundo se hace cada día más pequeño. Al principio era tan grande que le tenía miedo. '  
-         ' Corría y corría y por cierto que me alegraba ver esos muros, a diestra y siniestra, en la distancia. ' 
-         ' Pero esas paredes se estrechan tan rápido que me encuentro en el último cuarto y ahí en el rincón está '  
-         ' la trampa sobre la cual debo pasar. Todo lo que debes hacer es cambiar de rumbo dijo el gato...y se lo comió. ' 
-         '  '
-         ' Franz Kafka.'
+st.subheader("El otro – Jorge Luis Borges")
+st.write('Borges se encuentra con él mismo, más joven, sentado en un banco junto al río.
+Hablan un rato, y poco a poco ambos se convencen de que no están soñando.
+Cuando termina la conversación, cada uno vuelve a su tiempo, sabiendo que su encuentro fue real y a la vez imposible.
         
         )
            
